81173f4b17d9cd4dedcf419bb9c47e666a5c26bc
choi <choi@localhost>
2023-06-20 09:44:52 +0900

    ***

---
+++ src/user/UserDTO.java
@@ -0,0 +1,54 @@
+package user;
+//dto = date transfer object
+
+public class UserDTO {
+	String name;
+	String id;
+	String password;
+	String grade;
+	String nickname;
+	String email;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getGrade() {
+		return grade;
+	}
+	public void setGrade(String grade) {
+		this.grade = grade;
+	}
+	public String getNickname() {
+		return nickname;
+	}
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	//getter 현재 기록된데이터를 가져옮
+	//setter 데이터를 기록하는 것
+	
+	
+}
